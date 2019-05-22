518440849db4960136b298aea5452110b04204c6
elnataoliveira <elnataoliveira@gmail.com>
2019-05-22 20:22:05 -0300

    alter database to mysql

--- app/settings.py
+++ app/settings.py
@@ -78,10 +78,22 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'traderdb',
+        'USER': 'trader',
+        'PASSWORD': 'tradert2d',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
